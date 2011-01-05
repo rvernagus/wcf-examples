6ac898d6ec9688f95cdc214704d14239d88b5861
Ray Vernagus <rwvernagus@cmsenergy.com>
2011-01-05 15:39:46 -0500

    Added Common module and two Data Contract examples.

---
+++ FSharp/Data Contracts/Data Contract Format.fsx
@@ -0,0 +1,40 @@
+#load @"..\Common.fsx"
+#r "System.ServiceModel"
+open Common
+open System
+open System.ServiceModel
+open System.ServiceModel.Description
+open System.ServiceModel.Dispatcher
+Console.Clear()
+
+
+[<ServiceContract>]
+type IContract =
+    [<OperationContract>]
+    [<DataContractFormat(Style = OperationFormatStyle.Rpc)>]
+    abstract RpcOperation : string -> string
+
+    [<OperationContract>]
+    [<DataContractFormat(Style = OperationFormatStyle.Document)>]
+    abstract DocumentOperation : string -> string
+
+
+[<PrintMessagesToConsole>]
+type Service() =
+    interface IContract with
+        member this.RpcOperation(name) =
+            sprintf "Hi, %s!" name
+        
+        member this.DocumentOperation(name) =
+            sprintf "Hi, %s!" name
+
+
+let host = new ServiceHost(typeof<Service>, new Uri("net.tcp://localhost"))
+host.Open()
+
+let proxy = ChannelFactory<IContract>.CreateChannel(host.Description.Endpoints.[0].Binding, host.Description.Endpoints.[0].Address)
+proxy.RpcOperation("You")
+proxy.DocumentOperation("You")
+
+(proxy :?> ICommunicationObject).Close()
+host.Close()
