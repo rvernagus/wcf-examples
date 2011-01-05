6e4f18924d9e929e0765a1c95a2ce1ab38b20ad9
Ray Vernagus <rwvernagus@cmsenergy.com>
2011-01-05 07:54:11 -0500

    Revised SynchornizationContext example.

--- FSharp/Concurrency/SynchronizationContext.fsx
+++ FSharp/Concurrency/SynchronizationContext.fsx
@@ -1,16 +1,42 @@
+open System
+open System.Collections.Concurrent
 open System.Threading
-
-let runSync() =
-    let context = SynchronizationContext.Current
-    let doWork() =
-        context.Post((fun state ->
-            printfn "Worker Thread: %d" Thread.CurrentThread.ManagedThreadId), null)
-    let t = new Thread(doWork)
-    t.Start()
+open System.Threading.Tasks
 
 
-SynchronizationContext.SetSynchronizationContext(new SynchronizationContext())
-printfn "Main thread on thread #%d" Thread.CurrentThread.ManagedThreadId
-let st = runSync()
-printfn "Press any key to exit..."
-System.Console.ReadKey(true) |> ignore
+type MySynchronizationContext() as this =
+    inherit SynchronizationContext()
+
+    let workToDo = new BlockingCollection<SendOrPostCallback * obj>()
+    let thread = new Thread(new ThreadStart(this.DoWork))
+    do thread.Start()
+    do printfn "Synchronizing work on thread #%d" thread.ManagedThreadId
+
+    member private this.DoWork() =
+        let workEnum = workToDo.GetConsumingEnumerable()
+        for item in workEnum do
+            Thread.Sleep(1) // why is this necessary?
+            let d, state = item
+            d.Invoke(state)
+
+    override this.Send(d, state) =
+        workToDo.Add((d, state))
+
+    override this.Post(d, state) =
+        workToDo.Add((d, state))
+
+
+printfn "Main thread is #%d" Thread.CurrentThread.ManagedThreadId
+
+let doWork = new Action(fun _ ->
+    SynchronizationContext.Current.Send((fun _ ->
+        printfn "  Work done on Thread #%d" Thread.CurrentThread.ManagedThreadId), null))
+
+let syncContext = new MySynchronizationContext()
+SynchronizationContext.SetSynchronizationContext(syncContext)
+
+let options = new ParallelOptions()
+options.TaskScheduler <- TaskScheduler.FromCurrentSynchronizationContext()
+Parallel.Invoke(options, Array.create 10 doWork)
+
+Console.ReadLine()
