1a29a4e104c6e6505aac7cfc33c9153b42222ed9
Ray Vernagus <rwvernagus@cmsenergy.com>
2011-01-07 08:33:26 -0500

    Added IXmlSerializable example.

---
+++ FSharp/Data Contracts/IXmlSerializable.fsx
@@ -0,0 +1,104 @@
+#r "System.Xml"
+#load @"..\Common.fsx"
+open Common
+open System
+open System.IO
+open System.Xml
+open System.Xml.Schema
+open System.Xml.Serialization
+open System.Runtime.Serialization
+open System.ServiceModel
+open System.ServiceModel.Description
+
+
+[<DataContract(Name = "Item", Namespace = "http://schemas.myexample.org")>]
+type Item =
+    { [<DataMember>] mutable Name : string }
+
+
+[<XmlSchemaProvider("GetSchema")>]
+type ItemSerializer(item : Item) =
+    let mutable item' = item
+    let ns = "http://schemas.myexample.org"
+    
+    new() = ItemSerializer({ Name = "" })
+    
+    member this.Item
+        with get() = item'
+        and set v = item' <- v
+
+    static member GetSchema(schemaSet : XmlSchemaSet) =
+        let ns = "http://schemas.myexample.org"
+        let schemaString = sprintf "<xs:schema xmlns:tns='%s' xmlns:xs='http://www.w3.org/2001/XMLSchema' targetNamespace='%s' elementFormDefault='qualified' attributeFormDefault='unqualified'><xs:complexType name='Item'><xs:sequence><xs:element name='Name' type='xs:string' nillable='false'/></xs:sequence></xs:complexType></xs:schema>" ns ns
+
+        let schema = XmlSchema.Read(new StringReader(schemaString), null)
+        schemaSet.XmlResolver <- new XmlUrlResolver()
+        schemaSet.Add(schema) |> ignore
+        new XmlQualifiedName("Item", ns)
+
+    interface IXmlSerializable with
+        member this.GetSchema() =
+            raise (new NotImplementedException())
+        
+        member this.ReadXml(reader : XmlReader) =
+            let item = { Name = "" }
+            while reader.IsStartElement() do
+                reader.MoveToContent() |> ignore
+                reader.Read() |> ignore
+                if reader.IsStartElement("Name", ns) then
+                    reader.MoveToContent() |> ignore
+                    item.Name <- reader.ReadString()
+                    reader.MoveToContent() |> ignore
+                    reader.ReadEndElement()
+                    
+            this.Item <- item
+        
+        member this.WriteXml(writer : XmlWriter) =
+            writer.WriteElementString("Name", ns, this.Item.Name)
+
+
+[<ServiceContract(Name = "IMyContract")>]
+type IMyContract =
+    [<OperationContract>]
+    abstract MyMethod : ItemSerializer -> unit // Note different signature in server vs. client contract
+    
+    [<OperationContract>]
+    abstract MyOtherMethod : unit -> ItemSerializer
+
+
+[<ServiceContract(Name = "IMyContract")>]
+type IMyContractClient =
+    [<OperationContract>]
+    abstract MyMethod : Item -> unit
+    
+    [<OperationContract>]
+    abstract MyOtherMethod : unit -> Item
+
+
+[<ServiceBehavior(IncludeExceptionDetailInFaults = true)>]
+type MyService() =
+    interface IMyContract with
+        member this.MyMethod(itemSerializer) =
+            let item = itemSerializer.Item
+            printfn "Service Read: %A" item
+        
+        member this.MyOtherMethod() =
+            let item = { Name = "Server Item" }
+            new ItemSerializer(item)
+
+
+
+let host = new ServiceHost(typeof<MyService>, new Uri("net.tcp://localhost"))
+host.Open()
+
+let proxy = ChannelFactory<IMyContractClient>.CreateChannel(host.Description.Endpoints.[0].Binding, host.Description.Endpoints.[0].Address)
+
+// Client uses default DataContractSerializer
+// Server manually deserializes
+let item = { Name = "Client Item" }
+proxy.MyMethod(item)
+let serverItem = proxy.MyOtherMethod()
+printfn "Client Received: %A" serverItem
+
+(proxy :?> ICommunicationObject).Close()
+host.Close()
