b2c30fe2c574585e1ce3247061fc49d3a33ed24f
Ray Vernagus <rwvernagus@cmsenergy.com>
2011-01-07 08:19:45 -0500

    Added DataContract Equivalence example.

---
+++ FSharp/Data Contracts/DataContract Equivalence.fsx
@@ -0,0 +1,34 @@
+#load @"..\Common.fsx"
+open Common
+open System
+open System.Runtime.Serialization
+Console.Clear()
+
+
+[<DataContract(Name="Person", Namespace="")>]
+type Person =
+    { [<DataMember>]
+      mutable Name : string;
+      
+      [<DataMember>]
+      mutable Age : int }
+
+
+[<DataContract(Name="Person", Namespace="")>]
+type Employee =
+    { [<DataMember(Name="Name")>]
+      mutable FullName : string;
+      
+      [<DataMember(Name="Age")>]
+      mutable AgeInYears : int
+      
+      [<DataMember>]
+      mutable ServiceYears : int }
+
+
+let person = { Name = "Ray"; Age = 35 }
+let persondata = serialize person
+printfn "%s\n\n----------------------\n" persondata
+
+let emp = deserialize<Employee> persondata
+printfn "%A" emp
