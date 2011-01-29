96147415b48f347bc8b05dda564e774278989bf9
Good_man <devnull@localhost>
2011-01-28 20:48:53 -0500

    Added basic REST example

---
+++ CSharp/REST/Basic/Client/IRestContract.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace Client
+{
+    [ServiceContract]
+    public interface IRestContract
+    {
+        [OperationContract]
+        [WebGet]
+        string GetValue(string value);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST")]
+        string PostValue(string value);
+    }
+}
