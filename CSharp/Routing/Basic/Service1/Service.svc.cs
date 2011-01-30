70d247b2d5d1042ff78e3eb871a3728cde5e386b
Good_man <devnull@localhost>
2011-01-30 12:03:26 -0500

    Started Routing example

---
+++ CSharp/Routing/Basic/Service1/Service.svc.cs
@@ -0,0 +1,14 @@
+﻿namespace Service1
+{
+    public class Service : IContract
+    {
+        #region IContract Members
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        #endregion
+    }
+}
