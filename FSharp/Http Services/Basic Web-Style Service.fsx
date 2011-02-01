b6e61ca86ac02c4c2ed46cb0af7943a2cbf554f0
Ray Vernagus <rwvernagus@cmsenergy.com>
2011-02-01 08:32:12 -0500

    Revised HTTP Service examples.

--- FSharp/Http Services/Basic Web-Style Service.fsx
+++ FSharp/Http Services/Basic Web-Style Service.fsx
@@ -1,9 +1,43 @@
 // Adapted from http://msdn.microsoft.com/en-us/library/bb412178.aspx
 #r "System.ServiceModel"
 #r "System.ServiceModel.Web"
-#load "_Service.fsx"
+open System
+open System.ServiceModel
+open System.ServiceModel.Description
+open System.ServiceModel.Web
 
-printfn "Press enter to quit..."
-System.Console.ReadLine() |> ignore
 
-Service.close()
+[<ServiceContract>]
+type IService =
+    [<OperationContract>]
+    [<WebGet>]
+    abstract EchoWithGet : s : string -> string
+
+    [<OperationContract>]
+    [<WebInvoke>] // By default WebInvoke maps POST calls to the operation
+    abstract EchoWithPost : s : string -> string
+
+
+type Service() =
+    interface IService with
+        member this.EchoWithGet(s) =
+            printfn "  In EchoWithGet(s)"
+            sprintf "You said %s" s
+        
+        member this.EchoWithPost(s) = 
+            printfn "  In EchoWithPost(s)" 
+            (this :> IService).EchoWithGet(s)
+        
+
+let private host = new WebServiceHost(typeof<Service>, new Uri("http://localhost:8000"))
+host.AddServiceEndpoint(typeof<IService>, new WebHttpBinding(), "") |> ignore
+host.Description.Endpoints.[0].Behaviors.Add(new WebHttpBehavior(HelpEnabled=true))
+host.Open()
+
+printfn "Service is running"
+printfn "Visit http://localhost:8000/EchoWithGet?s=HttpMessage for example\n"
+printfn "Or visit http://localhost:8000/help for the Help page"
+printfn "Press <Enter> to exit..."
+Console.ReadLine()
+
+host.Close()
--- FSharp/Http Services/Call Web-Style Service with ChannelFactory.fsx
+++ FSharp/Http Services/Call Web-Style Service with ChannelFactory.fsx
@@ -1,10 +1,40 @@
+// Adapted from http://msdn.microsoft.com/en-us/library/bb412178.aspx
 #r "System.ServiceModel"
 #r "System.ServiceModel.Web"
-#load "_Service.fsx"
+open System
 open System.ServiceModel
 open System.ServiceModel.Description
+open System.ServiceModel.Web
 
-let factory = new ChannelFactory<Service.IService>(new WebHttpBinding(), "http://localhost:8000")
+
+[<ServiceContract>]
+type IService =
+    [<OperationContract>]
+    [<WebGet>]
+    abstract EchoWithGet : s : string -> string
+
+    [<OperationContract>]
+    [<WebInvoke>] // By default WebInvoke maps POST calls to the operation
+    abstract EchoWithPost : s : string -> string
+
+
+type Service() =
+    interface IService with
+        member this.EchoWithGet(s) =
+            printfn "  In EchoWithGet(s)"
+            sprintf "You said %s" s
+        
+        member this.EchoWithPost(s) = 
+            printfn "  In EchoWithPost(s)" 
+            (this :> IService).EchoWithGet(s)
+        
+
+let private host = new WebServiceHost(typeof<Service>, new Uri("http://localhost:8000"))
+host.AddServiceEndpoint(typeof<IService>, new WebHttpBinding(), "") |> ignore
+host.Description.Endpoints.[0].Behaviors.Add(new WebHttpBehavior(HelpEnabled=true))
+host.Open()
+
+let factory = new ChannelFactory<IService>(new WebHttpBinding(), "http://localhost:8000")
 factory.Endpoint.Behaviors.Add(new WebHttpBehavior())
 let channel = factory.CreateChannel()
 
@@ -16,4 +46,4 @@ printfn "Calling EchoWithPost via HTTP POST: "
 let sPost = channel.EchoWithPost("Hello, world")
 printfn "    Output: %s" sPost
 
-Service.close()
+host.Close()
