11d717e8b8218ae1701bbce5c9e911655a0bfb7a
Ray Vernagus <rwvernagus@cmsenergy.com>
2011-02-04 10:51:04 -0500

    Added Standard Endpoints example.

---
+++ FSharp/Endpoints/Standard Endpoints/Standard Endpoints/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.ServiceModel
+open System.ServiceModel.Web
+
+
+[<ServiceContract>]
+type IMyServiceContract =
+    [<OperationContract>]
+    [<WebGet>]
+    abstract MyOperation : unit -> string
+
+
+type MyService() =
+    interface IMyServiceContract with
+        member this.MyOperation() =
+            "Hi!"
+
+
+let host = new ServiceHost(typeof<MyService>)
+host.Open()
+
+printfn "The following endpoints were created:"
+for ep in host.Description.Endpoints do
+    printfn "  Address: %A" ep.Address
+    printfn "  Binding: %A" ep.Binding
+    printfn "---------------------"
+
+Console.ReadLine() |> ignore
+host.Close()
