128f40a08e16e80893cc340caa2c10dfb4d65f07
Good_man <devnull@localhost>
2011-02-11 20:50:50 -0500

    Json Service start

---
+++ CSharp/REST/JsonService/Service/IService1.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace Service
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebGet]
+        string GetData(int value);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST")]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+    }
+
+
+    [DataContract]
+    public class CompositeType
+    {
+        private bool _boolValue = true;
+        private string _stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return _boolValue; }
+            set { _boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return _stringValue; }
+            set { _stringValue = value; }
+        }
+    }
+}
